b60e99d3a269ad6a27f6d4a8377e0d720241260b
Rice-03 <raeezahmed03@gmail.com>
2026-05-06 15:28:52 +0200

    Check

--- simulationv1.py
+++ simulationv1.py
@@ -1,4 +1,4 @@
-#satellite simulation v1.0 (one Nominal pass simulation only)
+#satellite simulation v1.1 (one Nominal pass simulation only)
 #this code only simulates the change distance between the satellite and ground station change over time
 #provides a range vs time sequence
 
@@ -8,8 +8,9 @@ from orbdetpy import (configure, add_station, MeasurementType)
 from orbdetpy.conversion import get_J2000_epoch_offset
 #runs the forwards orbit propagation and generates simulated measurements
 from orbdetpy.propagation import propagate_orbits
+import numpy as np
 
-# Setting up simulation settings
+# Setting up simulatiodn settings
 cfg = configure(rso_mass=2500.0, prop_start=get_J2000_epoch_offset("2019-05-01T00:00:00"),
                 prop_initial_state=[-23183898.259, 35170229.755, 43425.075, -2566.938, -1692.19, 138.948],
                 prop_end=get_J2000_epoch_offset("2019-05-01T01:00:00"), prop_step=300.0, sim_measurements=True)
@@ -23,8 +24,19 @@ cfg.measurements[MeasurementType.RANGE].error[:] = [10.0]
 # Propagate orbits and generate measurements
 meas = list(m for m in propagate_orbits([cfg])[0].array)
 
-# Inspect the output
-print("Number of measurements:", len(meas))
-print("\nFirst measurement object:")
-print("Satellite: RBH-SAT1 ")
-print(meas[-1])
+#speed of light constant
+c = 299792458.0
+
+#placeholders for arrays:
+time = []
+ranges = []
+delays = []
+
+for m in meas:
+    t = m.time
+    r = m.values[0]
+    d = r/c
+
+    times.append(t)
+    ranges.append(r)
+    delays.append(d)
